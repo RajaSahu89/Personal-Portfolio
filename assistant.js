fe25e740277788aac286c56de2bf9187b89e7bfd
RajaSahu89 <rajasaw0657@gmail.com>
2026-09-17 15:52:29 +0530

    Update assistant.js

--- assistant.js
+++ assistant.js
@@ -55,7 +55,7 @@ const INTENTS = [
   },
   {
     keywords: ["education", "degree", "college", "university", "bca", "cgpa", "gpa", "school", "study", "studying",
-      "graduate", "graduation", "semester", "academic", "qualification"],
+      "graduate", "graduation", "semester", "Education", "academic", "qualification"],
     reply: () => RAJA_KB.education
   },
   {
